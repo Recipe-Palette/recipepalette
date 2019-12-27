c41738593f0650e4d41210974ce8d7f1d89f3aa4
Peter Garrow <garrow.peter@gmail.com>
2019-12-27 10:36:52 -0700

    Make recipe page layout more responsive

--- src/pages/recipe-page.js
+++ src/pages/recipe-page.js
@@ -1,7 +1,8 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
+import { useState } from 'react'
 import { Link } from 'gatsby'
-import { Flex } from '@theme-ui/components'
+import { Flex, Divider } from '@theme-ui/components'
 import { FiClock } from 'react-icons/fi'
 import Fraction from 'fraction.js'
 
@@ -15,17 +16,18 @@ const Icons = ({ recipe }) => (
   <div
     sx={{
       my: `2`,
-      mb: [`3`, `inherit`],
+      mb: `3`,
+
       order: 1,
-      justifyContent: [`space-evenly`, `initial`],
-      width: `100%`,
+      justifyContent: [`space-evenly`, `space-between`],
+      width: [`100%`, `50%`],
       display: [`grid`, `flex`],
       gridTemplateColumns: `repeat(3, 1fr)`,
+      alignSelf: `flex-start`,
+      ml: [`0`, `3`],
     }}
   >
-    <Flex
-      sx={{ mr: [`0`, `4`], alignItems: `center`, justifyContent: `center` }}
-    >
+    <Flex sx={{ mr: [`0`], alignItems: `center`, justifyContent: `center` }}>
       <Heart filled={recipe.hearted} size="2em" />{' '}
       <h2 sx={{ my: `0`, ml: `1` }}>{recipe.hearts}</h2>
     </Flex>
@@ -33,82 +35,169 @@ const Icons = ({ recipe }) => (
       <Copy filled={recipe.copied} size="2em" />{' '}
       <h2 sx={{ my: `0`, ml: `1` }}>{recipe.copies}</h2>
     </Flex>
-    <Flex
-      sx={{ display: [`inherit`, `none`, `none`], justifyContent: `center` }}
-    >
+    <Flex sx={{ justifyContent: `center` }}>
       <Bookmark filled={recipe.bookmarked} size="2em" />
     </Flex>
   </div>
 )
 
-const Timing = ({ recipe }) => (
+// const Timing = ({ recipe }) => (
+//   <Flex
+//     sx={{
+//       flexDirection: [`row`, `column`],
+//       mt: [`3`, `2`],
+//       // justifyContent: `space-evenly`,
+//       alignItems: [`center`, `initial`],
+//     }}
+//   >
+//     <FiClock
+//       size="2rem"
+//       sx={{ display: [`initial`, `none`], justifySelf: `flex-start`, mr: `2` }}
+//     />
+//     <Flex
+//       sx={{
+//         flexDirection: [`row`, `column`],
+//         justifyContent: [`space-evenly`, `initial`],
+//         width: `100%`,
+//       }}
+//     >
+//       <h2
+//         sx={{
+//           my: `2`,
+//           fontSize: [`2`, `4`],
+//           width: `100%`,
+//           textAlign: [`center`, `left`],
+//           borderRight: [`1px solid #999`, `none`],
+//         }}
+//       >
+//         Prep<span sx={{ display: [`none`, `initial`] }}> Time</span>:{' '}
+//         {convertTime(recipe.prep_time)}
+//       </h2>
+//       <h2
+//         sx={{
+//           my: `2`,
+//           fontSize: [`2`, `4`],
+//           width: `100%`,
+//           textAlign: [`center`, `left`],
+//           borderRight: [`1px solid #999`, `none`],
+//         }}
+//       >
+//         Cook<span sx={{ display: [`none`, `initial`] }}> Time</span>:{' '}
+//         {convertTime(recipe.cook_time)}
+//       </h2>
+//       <h2
+//         sx={{
+//           my: `2`,
+//           fontSize: [`2`, `4`],
+//           width: `100%`,
+//           textAlign: [`center`, `left`],
+//         }}
+//       >
+//         Total<span sx={{ display: [`none`, `initial`] }}> Time</span>:{' '}
+//         {convertTime(recipe.time)}
+//       </h2>
+//     </Flex>
+//   </Flex>
+// )
+
+const TimingSmall = ({ recipe }) => (
   <Flex
     sx={{
-      flexDirection: [`row`, `column`],
-      mt: [`3`, `2`],
-      // justifyContent: `space-evenly`,
-      alignItems: [`center`, `initial`],
+      flexDirection: `row`,
+      mt: `3`,
+      alignItems: `center`,
     }}
   >
-    <FiClock
-      size="2rem"
-      sx={{ display: [`initial`, `none`], justifySelf: `flex-start`, mr: `2` }}
-    />
+    <FiClock size="2rem" sx={{ justifySelf: `flex-start`, mr: `2` }} />
     <Flex
       sx={{
-        flexDirection: [`row`, `column`],
-        justifyContent: [`space-evenly`, `initial`],
+        flexDirection: `row`,
+        justifyContent: `space-evenly`,
         width: `100%`,
       }}
     >
-      <h2 sx={{ my: `2`, display: [`none`, `initial`] }}>
-        Servings: {recipe.servings}
-      </h2>
+      <Flex
+        sx={{
+          flexDirection: `column`,
+          width: `100%`,
+          borderRight: `1px solid #999`,
+        }}
+      >
+        <h3 sx={{ textAlign: `center`, m: `0` }}>Prep</h3>
+        <p sx={{ textAlign: `center`, mb: `0` }}>
+          {convertTime(recipe.prep_time)}
+        </p>
+      </Flex>
+      <Flex
+        sx={{
+          flexDirection: `column`,
+          width: `100%`,
+          borderRight: `1px solid #999`,
+        }}
+      >
+        <h3 sx={{ textAlign: `center`, m: `0` }}>Cook</h3>
+        <p sx={{ textAlign: `center`, mb: `0` }}>
+          {convertTime(recipe.cook_time)}
+        </p>
+      </Flex>
 
-      <h2
+      <Flex
         sx={{
-          my: `2`,
-          fontSize: [`2`, `4`],
+          flexDirection: `column`,
           width: `100%`,
-          textAlign: [`center`, `left`],
-          borderRight: [`1px solid #999`, `none`],
         }}
       >
-        Prep<span sx={{ display: [`none`, `initial`] }}> Time</span>:{' '}
+        <h3 sx={{ textAlign: `center`, m: `0` }}>Total</h3>
+        <p sx={{ textAlign: `center`, mb: `0` }}>{convertTime(recipe.time)}</p>
+      </Flex>
+      {/* <h2
+        sx={{
+          my: `2`,
+          fontSize: `2`,
+          width: `100%`,
+          textAlign: `center`,
+          borderRight: `1px solid #999`,
+        }}
+      >
+        Prep<span sx={{ display: [`none`] }}> Time</span>:{' '}
         {convertTime(recipe.prep_time)}
-      </h2>
-      <h2
+      </h2> */}
+      {/* <h2
         sx={{
           my: `2`,
-          fontSize: [`2`, `4`],
+          fontSize: `2`,
           width: `100%`,
-          textAlign: [`center`, `left`],
-          borderRight: [`1px solid #999`, `none`],
+          textAlign: `center`,
+          borderRight: `1px solid #999`,
         }}
       >
-        Cook<span sx={{ display: [`none`, `initial`] }}> Time</span>:{' '}
+        Cook<span sx={{ display: [`none`] }}> Time</span>:{' '}
         {convertTime(recipe.cook_time)}
-      </h2>
-      <h2
+      </h2> */}
+      {/* <h2
         sx={{
           my: `2`,
-          fontSize: [`2`, `4`],
+          fontSize: `2`,
           width: `100%`,
-          textAlign: [`center`, `left`],
+          textAlign: `center`,
         }}
       >
-        Total<span sx={{ display: [`none`, `initial`] }}> Time</span>:{' '}
+        Total<span sx={{ display: [`none`] }}> Time</span>:{' '}
         {convertTime(recipe.time)}
-      </h2>
+      </h2> */}
     </Flex>
   </Flex>
 )
 
 const RecipePage = () => {
   const recipe = recipes[0]
+  const [servings] = useState(recipe.servings)
+
   const image =
     recipe.img ||
     'https://images.unsplash.com/photo-1499636136210-6f4ee915583e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80'
+
+  // const handleServingsChange = e => setServings(e.target.value)
 
   return (
     <Layout>
@@ -117,30 +206,49 @@ const RecipePage = () => {
           py: `4`,
         }}
       >
-        <h1 sx={{ mb: `1` }}>{recipe.name}</h1>
-        <p>by {recipe.author}</p>
+        <Flex sx={{ flexDirection: [`column`, `row`] }}>
+          <div sx={{ width: `100%` }}>
+            <h1 sx={{ mb: `1` }}>{recipe.name}</h1>
+            <p>by {recipe.author}</p>
+          </div>
+          {/* <div
+            sx={{
+              display: `flex`,
+              justifyContent: `flex-end`,
+              alignSelf: `flex-start`,
+              width: `50%`,
+            }}
+          > */}
+          <Icons recipe={recipe} />
+          {/* </div> */}
+        </Flex>
+        {/* <h1 sx={{ mb: `1` }}>{recipe.name}</h1> */}
+        {/* <p>by {recipe.author}</p> */}
         <div
           sx={{
             display: [`flex`, `grid`],
-            gridGap: `2`,
+            gridGap: `3`,
             gridTemplateColumns: `repeat(2, 1fr)`,
             flexDirection: `column`,
           }}
         >
-          <div sx={{ display: [`flex`, `none`] }}>
+          {/* <div sx={{ display: [`flex`, `none`] }}>
             <Icons recipe={recipe} />
-          </div>
+          </div> */}
 
-          <div
-            sx={{
-              width: [`100%`, null, `434px`],
-              height: `329px`,
-              background: `url(${image})`,
-              backgroundSize: `cover`,
-              borderRadius: `2`,
-              order: 2,
-            }}
-          />
+          <div>
+            <div
+              sx={{
+                width: `100%`,
+                height: `330px`,
+                background: `url(${image})`,
+                backgroundSize: `cover`,
+                borderRadius: `2`,
+                order: 2,
+              }}
+            />
+            <TimingSmall recipe={recipe} />
+          </div>
           <div
             sx={{
               order: 3,
@@ -161,44 +269,49 @@ const RecipePage = () => {
                 Edit recipe
               </Link>
             </Flex>
-            <Timing recipe={recipe} />
-            <div sx={{ display: [`none`, `flex`] }}>
+            {/* <Timing recipe={recipe} /> */}
+            {/* <div sx={{ display: [`none`, `flex`] }}>
               <Icons recipe={recipe} />
-            </div>
+            </div> */}
           </div>
         </div>
+        {/* <Divider /> */}
         <div>
-          <Flex>
-            <h2 sx={{ width: `100%`, my: `2` }}>Ingredients</h2>
-            <h2
-              sx={{
-                display: [null, `none`],
-                width: `100%`,
-                textAlign: `right`,
-                my: `2`,
-              }}
-            >
-              Servings: {recipe.servings}
-            </h2>
-          </Flex>
+          <h2 sx={{ width: `100%`, my: `2` }}>Ingredients</h2>
+          <h3
+            sx={{
+              // display: [null, `none`],
+              width: `100%`,
+              mt: `0`,
+              mb: `2`,
+            }}
+          >
+            Servings: {servings}
+          </h3>
 
           <ul sx={{ columnCount: 2, listStyle: `none`, pl: `0` }}>
             {recipe.ingredients.map((ingredient, index) => {
               let amount = new Fraction(ingredient.amount)
               amount = amount.toFraction(true)
               return (
-                <li key={index}>
+                <li
+                  key={index}
+                  sx={{ mb: `3`, display: `inline-block`, width: `100%` }}
+                >
                   {amount} {ingredient.unit} {ingredient.name}
                 </li>
               )
             })}
           </ul>
         </div>
+        <Divider />
         <div>
           <h2>Instructions</h2>
-          <ol>
+          <ol sx={{ pl: `3` }}>
             {recipe.instructions.map((instruction, index) => (
-              <li key={index}>{instruction}</li>
+              <li key={index} sx={{ mb: `3` }}>
+                {instruction}
+              </li>
             ))}
           </ol>
         </div>
