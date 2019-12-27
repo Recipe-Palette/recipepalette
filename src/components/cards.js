a0e1ed4b46386537e6b03f895f31e36f172f99cb
Peter Garrow <garrow.peter@gmail.com>
2019-12-26 23:47:31 -0700

    Add links to each recipe card

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
+import { Link } from 'gatsby'
 import { darken } from '@theme-ui/color'
 import { FiClock } from 'react-icons/fi'
 import { Heart, Bookmark, Copy } from './icons'
@@ -44,102 +45,104 @@ const RecipeCard = ({
 }) => {
   time = convertTime(time)
   return (
-    <Card
-      sx={{
-        display: `grid`,
-        gridTemplateRows: `125px auto 32px`,
-        gridRowGap: `1`,
-        position: `relative`,
-        minHeight: 150,
-        p: `3`,
-        backgroundColor: darken(`background`, 0.01),
-        transition: `0.3s all`,
-        '&:hover': {
-          backgroundColor: darken(`background`, 0.035),
-        },
-      }}
-    >
-      <div
+    <Link to="/recipe-page" sx={{ color: `text`, textDecoration: `none` }}>
+      <Card
         sx={{
-          height: 125,
-          borderRadius: `1`,
-          backgroundColor: `#DDD`,
-          backgroundImage: `url(${img})`,
-          backgroundSize: `cover`,
-        }}
-      />
-      <div
-        sx={{
-          mt: `2`,
+          display: `grid`,
+          gridTemplateRows: `125px auto 32px`,
+          gridRowGap: `1`,
+          position: `relative`,
+          minHeight: 150,
+          p: `3`,
+          backgroundColor: darken(`background`, 0.01),
+          transition: `0.3s all`,
+          '&:hover': {
+            backgroundColor: darken(`background`, 0.035),
+          },
         }}
       >
-        <span
+        <div
           sx={{
-            fontSize: `3`,
+            height: 125,
+            borderRadius: `1`,
+            backgroundColor: `#DDD`,
+            backgroundImage: `url(${img})`,
+            backgroundSize: `cover`,
+          }}
+        />
+        <div
+          sx={{
+            mt: `2`,
           }}
         >
-          {name}
-        </span>
-      </div>
-      <div
-        sx={{
-          display: `flex`,
-          flexDirection: `row`,
-          justifyContent: `space-between`,
-        }}
-      >
-        <div sx={{ display: `flex`, alignItems: `center` }}>
-          <Heart size={20} filled={hearted} />
           <span
             sx={{
-              fontSize: `2`,
-              ml: `2`,
+              fontSize: `3`,
             }}
           >
-            {hearts}
+            {name}
           </span>
         </div>
-        <div sx={{ display: `flex`, alignItems: `center`, ml: `3` }}>
-          <Copy size={20} filled={copied} />
-          <span
-            sx={{
-              fontSize: `2`,
-              ml: `2`,
-            }}
-          >
-            {copies}
-          </span>
+        <div
+          sx={{
+            display: `flex`,
+            flexDirection: `row`,
+            justifyContent: `space-between`,
+          }}
+        >
+          <div sx={{ display: `flex`, alignItems: `center` }}>
+            <Heart size={20} filled={hearted} />
+            <span
+              sx={{
+                fontSize: `2`,
+                ml: `2`,
+              }}
+            >
+              {hearts}
+            </span>
+          </div>
+          <div sx={{ display: `flex`, alignItems: `center`, ml: `3` }}>
+            <Copy size={20} filled={copied} />
+            <span
+              sx={{
+                fontSize: `2`,
+                ml: `2`,
+              }}
+            >
+              {copies}
+            </span>
+          </div>
+          <div sx={{ display: `flex`, alignItems: `center`, ml: `3` }}>
+            <FiClock size={20} />
+            <span
+              sx={{
+                fontSize: `2`,
+                ml: `2`,
+              }}
+            >
+              {time}
+            </span>
+          </div>
         </div>
-        <div sx={{ display: `flex`, alignItems: `center`, ml: `3` }}>
-          <FiClock size={20} />
-          <span
-            sx={{
-              fontSize: `2`,
-              ml: `2`,
-            }}
-          >
-            {time}
-          </span>
+        <div
+          sx={{
+            borderRadius: 50,
+            backgroundColor: `background`,
+            border: theme => `1px solid ${theme.colors.border}`,
+            position: `absolute`,
+            width: 36,
+            height: 36,
+            right: 24,
+            top: 116,
+            display: `flex`,
+            alignItems: `center`,
+            justifyContent: `center`,
+          }}
+        >
+          <Bookmark size={24} filled={bookmarked} />
         </div>
-      </div>
-      <div
-        sx={{
-          borderRadius: 50,
-          backgroundColor: `background`,
-          border: theme => `1px solid ${theme.colors.border}`,
-          position: `absolute`,
-          width: 36,
-          height: 36,
-          right: 24,
-          top: 116,
-          display: `flex`,
-          alignItems: `center`,
-          justifyContent: `center`,
-        }}
-      >
-        <Bookmark size={24} filled={bookmarked} />
-      </div>
-    </Card>
+      </Card>
+    </Link>
   )
 }
 
