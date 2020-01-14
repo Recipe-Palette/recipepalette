cb7c93a5e5cfb0d348121c0335377fcb2bbf6300
Peter Garrow <garrow.peter@gmail.com>
2020-01-14 11:00:02 -0700

    Add locatino to add-recipe and update buttons to match theme-ui

--- src/pages/add-recipe.js
+++ src/pages/add-recipe.js
@@ -5,8 +5,8 @@ import Layout from '../components/layout'
 import RecipeForm from '../components/recipe-form'
 import Title from '../components/title'
 
-const AddRecipe = () => (
-  <Layout>
+const AddRecipe = ({ location }) => (
+  <Layout location={location}>
     <Title>Create new recipe</Title>
     <RecipeForm />
   </Layout>
