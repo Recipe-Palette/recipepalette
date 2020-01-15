5304575524a3a587515d6d2406333c5cb9c28b76
Peter Garrow <garrow.peter@gmail.com>
2020-01-15 14:14:15 -0700

    Change favicon using gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,17 @@ module.exports = {
     `gatsby-plugin-theme-ui`,
     `gatsby-plugin-auth`,
     `gatsby-plugin-apollo`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Recipe Palette`,
+        short_name: `Recipe Palette`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#eb7b15`,
+        display: `standalone`,
+        icon: `src/images/logo.png`,
+      },
+    },
   ],
 }
