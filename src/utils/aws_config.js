c97a71f9a17742a351dd761c81155e8b8b67ef8b
James Summers <james.summers@vivint.com>
2020-01-18 14:00:48 -0700

    aws s3 config file

---
+++ src/utils/aws_config.js
@@ -0,0 +1,16 @@
+exports.S3_ACCESS_KEY_ID =
+  process.env.S3_ACCESS_KEY_ID || 'AKIA4HWLI5566U6UPBYA'
+exports.S3_SECRET_ACCESS_KEY_ID =
+  process.env.S3_SECRET_ACCESS_KEY_ID ||
+  '59//i8mUJ6g5pHnUqQU4zS1Teh1qh7ztaXo4fhQe'
+exports.S3_ENDPOINT =
+  process.env.S3_ENDPOINT ||
+  'http://recipepaletteimages.s3-website-us-west-2.amazonaws.com'
+exports.S3_BUCKET = process.env.S3_BUCKET || 'recipepaletteimages'
+
+// S3 INFO
+// User: image_usr
+// Access Key: AKIA4HWLI5566U6UPBYA
+// Secret: 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
+// Bucket: recipepaletteimages
+// Endpoint: http://recipepaletteimages.s3-website-us-west-2.amazonaws.com (edited)
