8ccaf69c04884d7e622e2822cc3fa1bac9b475c6
Peter Garrow <garrow.peter@gmail.com>
2020-01-18 22:13:24 -0700

    Add preview to the image dropzone

--- src/components/recipe-form.js
+++ src/components/recipe-form.js
@@ -1,13 +1,13 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import { Fragment } from 'react'
+import { Fragment, useState, useEffect } from 'react'
 import { Formik, Form, FieldArray } from 'formik'
 import { Label, Input, Textarea, Button, Select } from '@theme-ui/components'
 import { FiTrash2, FiPlus } from 'react-icons/fi'
 import Cleave from 'cleave.js/react'
 import { useDropzone } from 'react-dropzone'
 
-const units = [
+const UNITS = [
   '---',
   'tsp',
   'tsbp',
@@ -21,7 +21,7 @@ const units = [
 
 const UnitDropdown = props => (
   <Select {...props}>
-    {units.map((unit, index) => (
+    {UNITS.map((unit, index) => (
       <option key={index}>{unit}</option>
     ))}
   </Select>
@@ -39,21 +39,57 @@ const parseTime = rawTime => {
   return total
 }
 
-function ImageDropzone() {
-  const { getRootProps, getInputProps } = useDropzone()
+const Previews = () => {
+  const [files, setFiles] = useState([])
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: 'image/*',
+    onDrop: acceptedFiles => {
+      setFiles(
+        acceptedFiles.map(file => ({
+          ...file,
+          preview: URL.createObjectURL(file),
+        }))
+      )
+    },
+    multiple: false,
+  })
 
-  // const files = acceptedFiles.map(file => (
-  //   <li key={file.path}>
-  //     {file.path} - {file.size} bytes
-  //   </li>
-  // ))
+  const thumbs = files.map((file, index) => (
+    <div
+      key={index}
+      sx={{
+        display: `flex`,
+        justifyContent: `center`,
+        p: `3`,
+        maxHeight: ``,
+      }}
+    >
+      =
+      <img
+        src={file.preview}
+        alt={file.name}
+        sx={{
+          maxWidth: `100%`,
+          maxHeight: [`400px`, `auto`],
+        }}
+      />
+    </div>
+  ))
+
+  useEffect(
+    () => () => {
+      files.forEach(file => URL.revokeObjectURL(file.preview))
+    },
+    [files]
+  )
 
   return (
-    <section
+    <div
       sx={{
         display: `flex`,
         flexDirection: `column`,
         mt: `4`,
+        backgroundColor: `#fafafa`,
         '& > p': {
           fontSize: `1rem`,
         },
@@ -86,13 +122,23 @@ function ImageDropzone() {
         }}
       >
         <input {...getInputProps()} />
-        <p>Drag 'n' drop some images here, or click to select images</p>
+        <p sx={{ mb: `0` }}>
+          Drag 'n' drop an image here, or click to select an image
+        </p>
       </div>
-      {/* <aside>
-        <h4>Images</h4>
-        <ul>{files}</ul>
-      </aside> */}
-    </section>
+      <div
+        sx={{
+          display: `flex`,
+          justifyContent: `center`,
+          borderColor: `#eeeeee`,
+          borderStyle: `solid`,
+          borderWidth: files.length > 0 ? `2px` : `0px`,
+          borderTop: `none`,
+        }}
+      >
+        {thumbs}
+      </div>
+    </div>
   )
 }
 
@@ -109,171 +155,179 @@ const RecipeForm = ({
   servings = '',
   image_url = '',
   privateRecipe = false,
-}) => (
-  <div sx={{ width: [`100%`, `480px`], margin: `0 auto` }}>
-    <Formik
-      initialValues={{
-        name,
-        ingredients,
-        instructions,
-        prep_time,
-        cook_time,
-        servings,
-        image_url,
-        privateRecipe,
-      }}
-      onSubmit={values => {
-        console.log(values)
-        console.log(parseTime(values.prep_time))
-        console.log(parseTime(values.cook_time))
+}) => {
+  // const { userId } = useAuth()
+  return (
+    <div sx={{ width: [`100%`, `480px`], margin: `0 auto` }}>
+      <Formik
+        initialValues={{
+          name,
+          ingredients,
+          instructions,
+          prep_time,
+          cook_time,
+          servings,
+          image_url,
+          privateRecipe,
+        }}
+        onSubmit={values => {
+          console.log(values)
+          console.log(parseTime(values.prep_time))
+          console.log(parseTime(values.cook_time))
 
-        setTimeout(() => {
-          // eslint-disable-next-line no-alert
-          alert(JSON.stringify(values, null, 2))
-        }, 500)
-      }}
-    >
-      {({ values, handleChange }) => (
-        <Form>
-          <Label htmlFor="name">Recipe name</Label>
-          <Input
-            name="name"
-            type="text"
-            id="name"
-            value={values.name}
-            onChange={handleChange}
-          />
-          <FieldArray
-            name="ingredients"
-            render={arrayHelpers => (
-              <Fragment>
-                <Label>Ingredients</Label>
-                <div
-                  sx={{
-                    display: `grid`,
-                    gridTemplateColumns: `15% 20% 1fr 15px`,
-                    gridGap: `2`,
-                    alignItems: `center`,
-                  }}
-                >
-                  <small>Amount</small>
-                  <small>Unit</small>
-                  <small sx={{ gridColumn: `3 / 5` }}>Ingredient</small>
-                  {values.ingredients &&
-                    values.ingredients.length > 0 &&
-                    values.ingredients.map((ingredient, index) => (
-                      <Fragment key={index}>
-                        <Input
-                          type="number"
-                          inputMode="decimal"
-                          step={0.01}
-                          min={0}
-                          name={`ingredients.${index}.amount`}
-                          value={ingredient.amount}
-                          onChange={handleChange}
-                        />
-                        <UnitDropdown
-                          name={`ingredients.${index}.unit`}
-                          value={ingredient.unit}
-                          onChange={handleChange}
-                        />
-                        <Input
-                          name={`ingredients.${index}.name`}
-                          value={ingredient.name}
-                          onChange={handleChange}
-                        />
-                        <FiTrash2
-                          onClick={() => arrayHelpers.remove(index)}
-                          sx={{ cursor: `pointer` }}
-                        />
-                      </Fragment>
-                    ))}
-                </div>
-                <div
-                  sx={{ display: `flex`, justifyContent: `flex-end`, pt: `3` }}
-                >
-                  <Button
-                    type="button"
-                    onClick={() =>
-                      arrayHelpers.push({ name: '', amount: '', unit: '' })
-                    }
+          setTimeout(() => {
+            // eslint-disable-next-line no-alert
+            alert(JSON.stringify(values, null, 2))
+          }, 500)
+        }}
+      >
+        {({ values, handleChange }) => (
+          <Form>
+            <Label htmlFor="name">Recipe name</Label>
+            <Input
+              name="name"
+              type="text"
+              id="name"
+              value={values.name}
+              onChange={handleChange}
+            />
+            <FieldArray
+              name="ingredients"
+              render={arrayHelpers => (
+                <Fragment>
+                  <Label>Ingredients</Label>
+                  <div
                     sx={{
-                      variant: `buttons.icon`,
+                      display: `grid`,
+                      gridTemplateColumns: `15% 20% 1fr 15px`,
+                      gridGap: `2`,
+                      alignItems: `center`,
                     }}
                   >
-                    Add a ingredient <FiPlus sx={{ ml: `2` }} />
-                  </Button>
-                </div>
-              </Fragment>
-            )}
-          />
-          <Label htmlFor="instructions">Instructions</Label>
-          <Textarea
-            value={values.instructions}
-            id="instructions"
-            rows={6}
-            onChange={handleChange}
-            placeholder="Place each step on a new line"
-          />
-          {/* TODO:
+                    <small>Amount</small>
+                    <small>Unit</small>
+                    <small sx={{ gridColumn: `3 / 5` }}>Ingredient</small>
+                    {values.ingredients &&
+                      values.ingredients.length > 0 &&
+                      values.ingredients.map((ingredient, index) => (
+                        <Fragment key={index}>
+                          <Input
+                            type="number"
+                            inputMode="decimal"
+                            step={0.01}
+                            min={0}
+                            name={`ingredients.${index}.amount`}
+                            value={ingredient.amount}
+                            onChange={handleChange}
+                          />
+                          <UnitDropdown
+                            name={`ingredients.${index}.unit`}
+                            value={ingredient.unit}
+                            onChange={handleChange}
+                          />
+                          <Input
+                            name={`ingredients.${index}.name`}
+                            value={ingredient.name}
+                            onChange={handleChange}
+                          />
+                          <FiTrash2
+                            onClick={() => arrayHelpers.remove(index)}
+                            sx={{ cursor: `pointer` }}
+                          />
+                        </Fragment>
+                      ))}
+                  </div>
+                  <div
+                    sx={{
+                      display: `flex`,
+                      justifyContent: `flex-end`,
+                      pt: `3`,
+                    }}
+                  >
+                    <Button
+                      type="button"
+                      onClick={() =>
+                        arrayHelpers.push({ name: '', amount: '', unit: '' })
+                      }
+                      sx={{
+                        variant: `buttons.icon`,
+                      }}
+                    >
+                      Add a ingredient <FiPlus sx={{ ml: `2` }} />
+                    </Button>
+                  </div>
+                </Fragment>
+              )}
+            />
+            <Label htmlFor="instructions">Instructions</Label>
+            <Textarea
+              value={values.instructions}
+              id="instructions"
+              rows={6}
+              onChange={handleChange}
+              placeholder="Place each step on a new line"
+            />
+            {/* TODO:
               On submit, we'll need to convert the time values into minutes before
           */}
-          <div sx={{ display: `flex`, justifyContent: `space-between` }}>
-            <div sx={{ display: `flex`, flexDirection: `column` }}>
-              <Label htmlFor="prep_time">Prep Time</Label>
-              <Cleave
-                name="prep_time"
-                id="prep_time"
-                inputMode="numeric"
-                placeholder="hh:mm"
-                options={{ time: true, timePattern: ['h', 'm'] }}
-                value={values.prep_time}
-                onChange={handleChange}
-                sx={{
-                  variant: `forms.input`,
-                }}
-              />
+            <div sx={{ display: `flex`, justifyContent: `space-between` }}>
+              <div sx={{ display: `flex`, flexDirection: `column` }}>
+                <Label htmlFor="prep_time">Prep Time</Label>
+                <Cleave
+                  name="prep_time"
+                  id="prep_time"
+                  inputMode="numeric"
+                  placeholder="hh:mm"
+                  options={{ time: true, timePattern: ['h', 'm'] }}
+                  value={values.prep_time}
+                  onChange={handleChange}
+                  sx={{
+                    variant: `forms.input`,
+                  }}
+                />
+              </div>
+              <div sx={{ display: `flex`, flexDirection: `column`, px: `3` }}>
+                <Label htmlFor="cook_time">Cook Time</Label>
+                <Cleave
+                  name="cook_time"
+                  id="cook_time"
+                  inputMode="numeric"
+                  placeholder="hh:mm"
+                  options={{ time: true, timePattern: ['h', 'm'] }}
+                  value={values.cook_time}
+                  onChange={handleChange}
+                  sx={{
+                    variant: `forms.input`,
+                  }}
+                />
+              </div>
+              <div sx={{ display: `flex`, flexDirection: `column` }}>
+                <Label htmlFor="servings">Servings</Label>
+                <Input
+                  name="servings"
+                  inputMode="numeric"
+                  type="number"
+                  id="servings"
+                  value={values.servings}
+                  onChange={handleChange}
+                />
+              </div>
             </div>
-            <div sx={{ display: `flex`, flexDirection: `column`, px: `3` }}>
-              <Label htmlFor="cook_time">Cook Time</Label>
-              <Cleave
-                name="cook_time"
-                id="cook_time"
-                inputMode="numeric"
-                placeholder="hh:mm"
-                options={{ time: true, timePattern: ['h', 'm'] }}
-                value={values.cook_time}
-                onChange={handleChange}
-                sx={{
-                  variant: `forms.input`,
-                }}
-              />
-            </div>
-            <div sx={{ display: `flex`, flexDirection: `column` }}>
-              <Label htmlFor="servings">Servings</Label>
-              <Input
-                name="servings"
-                inputMode="numeric"
-                type="number"
-                id="servings"
-                value={values.servings}
-                onChange={handleChange}
-              />
-            </div>
-          </div>
-          <ImageDropzone />
-          {/* TODO:
+            {/* <ImageDropzone /> */}
+            <Previews />
+            {/* TODO:
               - Add tags
               - Add image input */}
-          <div sx={{ display: `flex`, justifyContent: `flex-end`, py: `3` }}>
-            <Button type="submit" sx={{ variant: `buttons.submit` }}>
-              Submit
-            </Button>
-          </div>
-        </Form>
-      )}
-    </Formik>
-  </div>
-)
+            <div sx={{ display: `flex`, justifyContent: `flex-end`, py: `3` }}>
+              <Button type="submit" sx={{ variant: `buttons.submit` }}>
+                Submit
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )
+}
 
 export default RecipeForm
