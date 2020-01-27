7d07383192d288c7849a84e86d34f0a3e3503788
Peter Garrow <garrow.peter@gmail.com>
2020-01-27 08:23:07 -0700

    Fix formatting for minutes

--- src/utils/parseTime.js
+++ src/utils/parseTime.js
@@ -13,8 +13,11 @@ const parseTime = rawTime => {
 const formatTime = rawMinutes => {
   const hours = Math.floor(rawMinutes / 60)
   const minutes = rawMinutes % 60
+  console.log(minutes)
 
-  return `${hours < 10 ? `0${hours}` : hours}:${minutes}`
+  return `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }`
 }
 
 export { parseTime, formatTime }
