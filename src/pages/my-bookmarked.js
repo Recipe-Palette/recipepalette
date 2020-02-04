b577448ed434b4df412c084e77251da307d1e068
Matthew Hamilton <somecid183@yahoo.com>
2020-02-04 12:33:50 -0700

    added hearted and bookmarked pages and updated the queries for them. They can only be accessed by typing in their path currently. My palette should get tabs

---
+++ src/pages/my-bookmarked.js
@@ -0,0 +1,71 @@
+/* eslint-disable react/display-name */
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import Title from '../components/title'
+import Layout from '../components/layout'
+import { NewCard, RecipeCard } from '../components/cards'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import { useAuth } from 'react-use-auth'
+
+import { bookmarkInformationFragment } from '../graphql/fragments'
+
+const bookmarkedQuery = gql`
+  query MyQuery($user_id: String!) {
+    recipes: recipe(
+      order_by: { latest: { created_at: desc } }
+      where: { bookmarks: { user_id: { _eq: $user_id } } }
+    ) {
+      id
+      image_url
+      latest_version
+      latest {
+        cook_time_minutes
+        prep_time_minutes
+        name
+        created_at
+        version
+      }
+      bookmarks(where: { user_id: { _eq: $user_id } }) {
+        ...BookmarkInformation
+      }
+    }
+  }
+  ${bookmarkInformationFragment}
+`
+
+export default ({ location }) => {
+  const { userId } = useAuth()
+  const { data: recipeData, loading } = useQuery(bookmarkedQuery, {
+    variables: { user_id: userId },
+  })
+  if (loading) {
+    return null
+  }
+
+  return (
+    <Layout location={location}>
+      <div
+        sx={{
+          py: `4`,
+        }}
+      >
+        <Title>My Bookmarked Recipes</Title>
+        <div
+          sx={{
+            display: `grid`,
+            gridTemplateColumns: [`repeat(auto-fit, minmax(275px, 1fr))`],
+            gridAutoFlow: `row`,
+            gridGap: `3`,
+            mb: `4`,
+          }}
+        >
+          <NewCard />
+          {recipeData.recipes.map((recipe, index) => (
+            <RecipeCard key={index} recipe={recipe} />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  )
+}
