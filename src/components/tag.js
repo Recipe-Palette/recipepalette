1b0e292392fbbb46c00fc169cecf2d624d6f5f24
Peter Garrow <garrow.peter@gmail.com>
2020-02-13 08:19:11 -0700

    Use theme-ui for the badges

--- src/components/tag.js
+++ src/components/tag.js
@@ -3,28 +3,7 @@ import { jsx } from 'theme-ui'
 import { Badge } from '@theme-ui/components'
 
 const TagBadge = ({ name }) => {
-  return (
-    <Badge
-      sx={{
-        cursor: `pointer`,
-        color: `background`,
-        borderWidth: `1px`,
-        borderColor: `primary`,
-        borderStyle: `solid`,
-        marginRight: `5px`,
-        backgroundColor: `primary`,
-        transition: `0.2s all`,
-        textDecoration: `none`,
-        fontSize: `1`,
-        px: `2`,
-        py: `1`,
-        borderRadius: `1`,
-        textAlign: `center`,
-      }}
-    >
-      {name}
-    </Badge>
-  )
+  return <Badge variant="primary">{name}</Badge>
 }
 
 export default TagBadge
