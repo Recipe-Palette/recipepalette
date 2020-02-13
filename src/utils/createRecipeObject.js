cf4bc614fe14054d2f71bd40c5bdc54b3de732fc
Peter Garrow <garrow.peter@gmail.com>
2020-02-13 08:22:13 -0700

    Add tags to createRecipeObject

--- src/utils/createRecipeObject.js
+++ src/utils/createRecipeObject.js
@@ -1,6 +1,7 @@
 import { parseTime } from './parseTime'
 
 const createRecipeObject = (values, recipe_id, userId, version, imageUrl) => {
+  const { tags } = values
   const instructions = values.instructions.split('\n')
   const prep_time_minutes = parseTime(values.prep_time)
   const cook_time_minutes = parseTime(values.cook_time)
@@ -35,6 +36,28 @@ const createRecipeObject = (values, recipe_id, userId, version, imageUrl) => {
 
   if (imageUrl.length > 0) {
     recipe.data.image_url = imageUrl
+  }
+
+  if (tags.length > 0) {
+    const tagData = {
+      data: tags.map(tag => ({
+        tag: {
+          data: {
+            name: tag,
+          },
+          on_conflict: {
+            constraint: 'tag_value_key',
+            update_columns: 'name',
+          },
+        },
+      })),
+      on_conflict: {
+        constraint: 'tag_recipe_tag_id_recipe_id_key',
+        update_columns: ['tag_id', 'recipe_id'],
+      },
+    }
+
+    recipe.data.tags = tagData
   }
 
   recipe.data.user_id = userId
