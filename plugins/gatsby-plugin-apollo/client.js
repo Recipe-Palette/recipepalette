f46c16232e5f704017c2468beb24fd563c90acde
Peter Garrow <garrow.peter@gmail.com>
2020-03-03 11:44:11 -0700

    remove hdaer

--- plugins/gatsby-plugin-apollo/client.js
+++ plugins/gatsby-plugin-apollo/client.js
@@ -4,15 +4,15 @@ import fetch from 'isomorphic-fetch'
 
 export const client = new ApolloClient({
   request: operation => {
-    const isBrowser = typeof window !== 'undefined'
-    const token = isBrowser ? localStorage.getItem('token') : null
+    // const isBrowser = typeof window !== 'undefined'
+    // const token = isBrowser ? localStorage.getItem('token') : null
     operation.setContext({
       uri:
         process.env.GATSBY_HASURA_URL ||
         'https://recipe-palette-dev.herokuapp.com/v1/graphql',
       headers: {
         'content-type': 'application/json',
-        Authorization: `Bearer ${token}`,
+        // Authorization: `Bearer ${token}`,
       },
       fetch,
     })
